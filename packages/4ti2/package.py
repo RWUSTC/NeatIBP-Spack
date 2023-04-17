7db178eda51d78c76fd55d577cf0ddf96ef7f3ca
RWUSTC <rwinters@mail.ustc.edu.cn>
2023-04-17 15:01:56 +0800

    new file:   packages/4ti2/package.py 	new file:   packages/flint/package.py 	new file:   packages/singular/package.py

---
+++ packages/4ti2/package.py
@@ -0,0 +1,43 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install 4ti2
+#
+# You can edit this file again by typing:
+#
+#     spack edit 4ti2
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class _4ti2(AutotoolsPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://4ti2.github.io"
+    url      = "https://4ti2.github.io/version_1.6/4ti2-1.6.tar.gz"
+    git      = "https://github.com/4ti2/4ti2.git"
+
+    # maintainers = ['github_user1', 'github_user2']
+
+    version('1.6.9', branch='master')
+    version('1.6', sha256='5edd106a3584408d89c58cb432f8b08eec472aa3277a35928523cdb4e43c769e')
+
+
+    depends_on("autoconf", type="build", when="@1.6.9 build_system=autotools")
+    depends_on("automake", type="build", when="@1.6.9 build_system=autotools")
+    depends_on("libtool", type="build", when="@1.6.9 build_system=autotools")
+
